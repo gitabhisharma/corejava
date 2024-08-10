905ab3a20db558c85233a3f76b3aeca88fce4e21
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-10 21:35:32 +0530

    Subterct

---
+++ Subterct.java
@@ -0,0 +1,17 @@
+public class Subterct{
+
+    //This line of code is diffine the Which Function to use
+    public static void main(String[] args) {
+        
+        //Agian Use intger type 
+        int a = 10;
+        int b = 5;
+
+        //and calculet the given number
+        int result = a - b;
+
+        //and print the result value
+        System.out.println(result);
+    }
+
+}
