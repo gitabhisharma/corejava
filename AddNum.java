f2a3643a9c18d12dfffe6180be6029fb255ca477
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-10 21:25:44 +0530

    Comment pass out

--- AddNum.java
+++ AddNum.java
@@ -1,9 +1,13 @@
 class AddNum {
     public static void main(String[] args) {
-        int a = 5;
+        
+        int a = 5; //int is a integer type of value that suport only real number
+        
         int b = 6;
-        int add = a + b;
-        System.out.println(add);
+        
+        int add = a + b; //this funcition is used to add the value given by
+        
+        System.out.println(add); // this code run and calculet the given value
     }
 
 }
--- HelloW.java
+++ HelloW.java
@@ -1,5 +1,9 @@
 public class HelloW {
     public static void main(String[] args) {
+
+        // This is our first code in core java
+
+        //This line of code print the Hello java World
         System.err.println("Hello java World");
     }
 }
