f357b574a8e0f1594f4ff90d95acaf61f43d245a
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-10 22:13:29 +0530

    divided a number

---
+++ Div.java
@@ -0,0 +1,11 @@
+public class Div {
+    public static void main(String[] args) {
+        
+        int a = 4;
+        int b = 2;
+
+        int div = a / b;
+
+        System.out.println(div);
+    }
+}
