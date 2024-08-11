3b383a3c43a19b28c4bd56a4c02e40121f8a244b
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-11 23:52:05 +0530

    EvenOrde Number

---
+++ EnvenOrde.java
@@ -0,0 +1,21 @@
+
+//Conditional statament :- To check the even or orde number
+
+public class EnvenOrde {
+    public static void main(String[] args) {
+        //Enter the value
+        int a = 8;
+
+        //Apply The Condition
+        if(a %2 == 0){
+            //Condition to check the value ture or false
+            System.out.println("Even Number");
+        }else{
+                //Outherwise
+                System.out.println("Orde Number");
+        }
+
+        
+
+    }
+}
