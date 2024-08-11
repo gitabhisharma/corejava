31b653b9f993f272811307d95cd760b6bb0177c4
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-11 22:51:36 +0530

    logical NOT

---
+++ logicalNOT.java
@@ -0,0 +1,17 @@
+
+//Logical OPerater :- NOT-!
+
+public class logicalNOT {
+    public static void main(String[] args){
+
+        //Enter the value
+        int x =10;
+        int y =7;
+
+        //operation logical not
+        boolean resule = x > y;
+
+        //print the value
+        System.out.println(!resule);
+    }
+}
