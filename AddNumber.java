810d592a802df490f94d82d2c12ec7a541ce318a
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-11 22:37:21 +0530

    logical operater

---
+++ AddNumber.java
@@ -0,0 +1,7 @@
+public class AddNumber {
+    
+    public static void main(String[] args) {
+        
+        int
+    }
+}
---
+++ logicalaAnd.java
@@ -0,0 +1,21 @@
+//Logical Operators :- And-&&
+
+public class logicalaAnd {
+    public static void main(String[] args) {
+        
+        //Enter the value
+        int x =10;
+        int y =7;
+
+        //And anouter value enter
+        int a = 10;
+        int b = 7;
+
+        //And write a logical opreater
+        boolean result = x >= y && a >= b;
+
+        //And print the result
+        System.out.println(result);
+
+    }
+}
