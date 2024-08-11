f6894b19cc06974117650b49bd666916b5cf1dc5
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-11 22:44:00 +0530

    Logical OR

---
+++ logicalOR.java
@@ -0,0 +1,21 @@
+
+//Logical Operater:- OR-||
+
+public class logicalOR {
+    public static void main(String[] args) {
+        
+        //Enter the value
+        int x =20;
+        int y = 24;
+
+        //Angian Enter the value
+        int a = 10;
+        int b = 7;
+
+        //Opration the Logical Operater OR
+        boolean result = x <= y || a <= b;
+
+        //Print the value
+        System.out.println(result);
+    }
+}
