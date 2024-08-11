d69471b5a63cca99aace756dab6132ad75db82cc
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-11 23:10:07 +0530

    Conditional Statament

---
+++ conditional.java
@@ -0,0 +1,17 @@
+
+//Conditional Statament 
+
+public class conditional {
+    public static void main(String[] args) {
+        
+        //Enter the value
+        int a = 10;
+
+        //Conditional Statamet apply here
+        if(a >= 12){
+            System.out.println("True");
+        }else{ //outher wise print here
+            System.out.println("False");
+        }
+    }
+}
