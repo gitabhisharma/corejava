641027fa9b8bdee9e4241a768319699d5a3e4bbf
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-12 13:45:44 +0530

    check the three value of gerater then

---
+++ ThreeValue.java
@@ -0,0 +1,23 @@
+
+//Conditional Statament :- Check the gerater than of three Number
+
+public class ThreeValue {
+    public static void main(String[] args) {
+        //Enter the value of user input
+        int a = 10;
+        int b = 23;
+        int c = 24;
+
+        //Let's Check the value of gerater than
+        if(a > b && a > c){
+            System.out.println("a is gerater then");
+        }else if(b > c){
+            System.out.println("b is the gerater then");
+        }else {
+            System.out.println("c is gerater then ");
+        }
+        
+
+    }
+
+}
