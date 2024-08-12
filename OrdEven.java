a0426b6ef2c82a520428cbb5f9757b22f0196f28
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-13 00:47:45 +0530

    checkthat

---
+++ OrdEven.java
@@ -0,0 +1,9 @@
+public class OrdEven{
+    public static void main(String[] args){
+        int a = 5;
+        int b = 4;
+        if(a %2 == 0 && b %2 == 0){
+            System.out.println(a);
+        }
+    }
+}
