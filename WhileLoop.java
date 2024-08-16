0b31b10aa80eda576ed011bc21a3487f3de32259
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-16 15:04:43 +0530

    loop

---
+++ WhileLoop.java
@@ -0,0 +1,18 @@
+
+//Loop Statement
+
+public class WhileLoop {
+    public static void main(String[] args) {
+        //Enter the value of your requriment
+        int i = 1;
+
+        //Check the conditions to Lop statement .
+        while (i <= 10) {
+
+            //Your print the value your are a given
+            System.out.println(i + "Hello Java World");
+            i++;
+            
+        }
+    }
+}
