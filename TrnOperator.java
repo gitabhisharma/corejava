cff35d8f3f0db40f5c8013dfbeed57de7f9186f3
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-16 13:31:54 +0530

    TrnOperator

---
+++ TrnOperator.java
@@ -0,0 +1,19 @@
+
+
+//TrnOperator use that in case
+
+
+public class TrnOperator {
+    public static void main(String[] args){
+        //Enter the value of your requriment
+        int n = 7;
+
+        //I use a int value you chose any vlue of your requriment
+        int result = 0;
+
+        result = n%2==0 ? 1 : 0; // Here that use a TrnOperator
+
+        //Print that.
+        System.out.println(result);
+    }
+}
