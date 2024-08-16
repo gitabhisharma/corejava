5d5c8df3c3d2acc5d5884c1fb7de0a081965e126
abhisharma <166649869+gitabhisharma@users.noreply.github.com>
2024-08-16 15:28:16 +0530

    DoWhileLoop

---
+++ DoWhileLoop.java
@@ -0,0 +1,16 @@
+
+//Do While Loop statement
+
+public class DoWhileLoop {
+    public static void main(String[] args) {
+        
+        int i = 1;
+
+        //DO loop statement here
+        do{
+            //
+            System.out.println( i + "Hello Java World");
+            i++;
+        }while(i <= 4); // while loop statement here
+    }
+}
